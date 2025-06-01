36d4042367c9dfae0cb1f7a131ed4438a6d2ece7
leandroNarruda <69996435+leandroNarruda@users.noreply.github.com>
2025-06-01 03:02:14 +0000

    Deploying to gh-pages from @ leandroNarruda/teste-ui@b075565bbf37bdb682ca92bebc882893d3fbc59c 🚀

--- assets/Button.stories-C3guw-xF.js
+++ assets/Button.stories-C3guw-xF.js
@@ -1,4 +1,4 @@
-import{f as b}from"./index-BZkcKs8Z.js";import{B as y}from"./Button-BdN_XYjr.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-D4lIrffr.js";const k={title:"Example/Button",component:y,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:b()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}};var s,t,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+import{fn as b}from"./index-Cf3xVBfy.js";import{B as y}from"./Button-C3XmvMm7.js";import"./jsx-runtime-BO8uF4Og.js";import"./index-D4H_InIO.js";const k={title:"Example/Button",component:y,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:b()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}};var s,t,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
   args: {
     primary: true,
     label: 'Button'
