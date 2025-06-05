308dd1e0a82c8e7c94fe5fb62fee67cf1009bb1b
leandroNarruda <69996435+leandroNarruda@users.noreply.github.com>
2025-06-05 18:56:30 +0000

    Deploying to gh-pages from @ leandroNarruda/teste-ui@2b2f70763b3400f5de61d036c929647a7f49df40 🚀

---
+++ assets/Checkbox.stories-BtHPY-bZ.js
@@ -0,0 +1,7 @@
+import{R as a}from"./index-D4H_InIO.js";import{C as m}from"./index-Bh1tIe-3.js";import"./jsx-runtime-BO8uF4Og.js";const d={title:"Example/Checkbox",component:m,tags:["autodocs"],parameters:{layout:"fullscreen"},decorators:[o=>a.createElement("div",{style:{padding:"3rem",maxWidth:"400px",margin:"0 auto"}},a.createElement(o))],args:{label:"Label2",name:"name",disabled:!1}},e={args:{label:"Aceitar termos e condições",name:"name",disabled:!1}};var r,s,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    label: 'Aceitar termos e condições',
+    name: 'name',
+    disabled: false
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const i=["Primary"];export{e as Primary,i as __namedExportsOrder,d as default};
